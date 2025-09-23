950b7609c79978bf98b2b940873468b6e9f187d8
Dwynne74 <ddbalbosa@gmail.com>
2025-09-22 21:00:05 -0600

    added back to top and nav active state

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ theme.addEventListener("click", () => {
   theme.classList.toggle("bi-sun");
   document.body.classList.toggle("dark");
 });
+
+const sections = document.querySelectorAll(".nav-section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 50;
+    if (window.scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
